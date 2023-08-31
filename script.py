3db571f5eabbc7d94f7d2d6f879a94adafd8ce99
Luan Menezes <luanmenezesmluiz@gmail.com>
2023-08-31 16:43:40 -0300

    Estudando sobre a biblioteca pandas e testando métodos que podem ser utilizados na análise de dados

--- script.py
+++ script.py
@@ -1,10 +1,22 @@
-import pandas as pd 
-import matplotlib.pyplot as plt
-import seaborn as sns
-from fpdf import FPDF
+import pandas as pd
+import os
 
-df = pd.read_csv("./arquivosExcel/CADASTRO_IES_2020.CSV", encoding='ISO-8859-1', sep=';', low_memory=False)
+""" df = pd.read_csv("./arquivosExcel/CADASTRO_IES_2020.CSV",
+                 encoding='ISO-8859-1', sep=';', low_memory=False)
 
 df.dropna(inplace=True)
 
-print(df.head())
+print(df.head()) """
+
+# Criando um dataframe (tabela) através de um arquivo de dados
+ies_df = pd.read_csv((os.getcwd() + "/arquivosExcel/CADASTRO_IES_2020.CSV"), encoding='ISO-8859-1', sep=';', low_memory=False)
+
+# Resumo de Visualização de Dados
+print(ies_df.head(10))  # Mostra as 10 primeiras linhas
+print(ies_df.shape)  # Mostra a quantidade de linhas e colunas
+print(ies_df.columns)  # Mostra o nome das colunas
+print(ies_df.dtypes)  # Mostra o tipo de dado de cada coluna
+print(ies_df.info())  # Mostra o resumo das colunas
+print(ies_df.describe())  # Mostra um resumo estatístico das colunas numéricas
+print(ies_df['CO_IES'].value_counts())  # Mostra a quantidade de valores únicos
+print(ies_df.tail(10))  # Mostra as 10 últimas linhas
