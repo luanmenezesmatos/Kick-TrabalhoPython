0c8f10edbfdfdc250d7445726c287c2f97638621
Luan Menezes <luanmenezesmluiz@gmail.com>
2023-09-05 16:28:46 -0300

    Deletando o .py para utilizar o Jupyter

--- script.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import os
-
-# Criando um dataframe (tabela) através de um arquivo de dados
-ies_df = pd.read_csv((os.getcwd() + "/arquivosExcel/CADASTRO_IES_2020.CSV"),
-                     encoding='ISO-8859-1', sep=';', low_memory=False)
-
-# Resumo de Visualização de Dados
-""" print(ies_df.head()) """
