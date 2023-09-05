68c04636035c2a752c9972586f1b228a687c5e9e
Luan Menezes <luanmenezesmluiz@gmail.com>
2023-09-05 16:27:59 -0300

    Fazendo algumas alterações e implementando gráficos

--- script.py
+++ script.py
@@ -1,19 +1,9 @@
 import pandas as pd
 import os
 
-""" df = pd.read_csv("./arquivosExcel/CADASTRO_IES_2020.CSV",
-                 encoding='ISO-8859-1', sep=';', low_memory=False)
-
-df.dropna(inplace=True)
-
-print(df.head()) """
-
 # Criando um dataframe (tabela) através de um arquivo de dados
 ies_df = pd.read_csv((os.getcwd() + "/arquivosExcel/CADASTRO_IES_2020.CSV"),
                      encoding='ISO-8859-1', sep=';', low_memory=False)
 
 # Resumo de Visualização de Dados
 """ print(ies_df.head()) """
-
-ies_uf = ies_df["NO_UF_IES"]
-print(ies_uf.head())
